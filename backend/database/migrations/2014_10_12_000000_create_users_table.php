61650438c6db9be9776e7ff2dde21bf31a4f35ce
MOohamedMahfouz <125674111+MOohamedMahfouz@users.noreply.github.com>
2024-02-28 11:51:31 +0300

    modify user

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('full_name')->nullable();
-            $table->string('email')->nullable();
+            $table->string('email')->unique();
             $table->string('phone_number')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
--- backend/routes/api/v1/user/user.php
+++ backend/routes/api/v1/user/user.php
@@ -1,4 +1,12 @@
 <?php
 
 
+use App\Http\Controllers\Api\V1\User\UserController;
+use Illuminate\Support\Facades\Route;
+
 require 'auth.php';
+require 'order.php';
+
+
+Route::put('update',[UserController::class,'update']);
+Route::get('profile',[UserController::class,'initialData']);
